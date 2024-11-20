ab64c9cf8a87094a021eee29df9ccc667a13ccc6
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-11-20 23:27:06 +0900

    input으로 입력 받기

---
+++ GSeek/GSeek.py
@@ -0,0 +1,20 @@
+# print("너 이름이 뭐니?")
+# name = input()
+# print("너의 이름은 " + name + "이구나")
+
+# 숫자 2개를 입력 받아서 출력하는 프로그램
+# a = input()
+# b = input()
+# print(a + b)
+
+# input()으로 받는 값은 문자열임 더하면 둘이 붙어 나옴
+# 문자열을 숫자로 바꾸는 방법
+# a = int(input())
+# b = int(input())
+# print(a + b)
+
+# 숫자 3개를 입력 받아 그 곱을 출력하는 프로그램
+a = int(input())
+b = int(input())
+c = int(input())
+print(a * b * c)
