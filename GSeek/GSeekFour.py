22b0bc6cd797b26e90eebd39cd529d0a1de02b2f
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-11-24 23:51:36 +0900

    random 모듈

--- GSeek/GSeekFour.py
+++ GSeek/GSeekFour.py
@@ -1,3 +1,5 @@
+import random
+
 print(2+8)
 print("안녕하세요", "Python")
 # , 을 넣어서 출력하면 띄어쓰기로 출력됨
@@ -63,6 +65,63 @@ print("""
 # 2의 7 제곱은 2**7
 
 # round(a/b , 1) a 나누기 b 했을 때 결과값 소수 자리수 1자리까지 출력
-r = float(input("원의 반지름을 입력해주세요 : \n"))
-print(f"원의 넓이는 {r * r * 3.14}")
-print(f"원의 넓이(거듭제곱) {round(r**2 * 3.14, 2)}")
+# r = float(input("원의 반지름을 입력해주세요 : \n"))
+# print(f"원의 넓이는 {r * r * 3.14}")
+# print(f"원의 넓이(거듭제곱) {round(r**2 * 3.14, 2)}")
+
+# su = int(input("숫자를 입력해주세요.\n"))
+# if su % 2 == 0 :
+#     print(su, "(은)는 짝수이다.")
+# else :
+#     print(su, "(은)는 홀수입니다.")
+
+# 합격/불합격 코드
+# pa = 80
+# score = int(input("당신의 점수를 입력하세요\n"))
+#
+# if score >= pa :
+#     print("합격입니다.")
+# else :
+#     print("불합격입니다.")
+
+# random() : 0부터 1사이의 부동소수점(float)숫자를 리턴
+print(random.random())
+
+# randint(최소, 최대) : 최소부터 최대까지의 중 임의의 정수를 리턴
+print(f"선택한 수는 : {random.randint(0, 5)} 입니다.")
+
+# uniform(최소, 최대) : 최소에서 최대까지 중 임의의 부동소수점(float)숫자를 리턴
+a = random.uniform(1.14, 36.5)
+print(f"1.14와 36.5 사이의 선택한 수 : {a} 입니다.")
+
+# randrange(시작, 끝, 간격) : 시작부터 끝까지 숫자 중에 지정된 간격의 숫자 중 리턴하고 간격 값은 선택 사항
+b = random.randrange(5, 20)
+print(f"5와 20 사이의 선택한 수 : {b} 입니다.")
+
+# shuffle(컬렉션) : 컬렉션의 값을 뒤섞어서 리턴
+c = ['apple', 'orange', 'book', 'cat', 'dog']
+random.shuffle(c)
+print(f"{c}")
+
+# choice(컬렉션) : 컬렉션의 값 중 임의의 값을 리턴
+print(f"{random.choice(c)}")
+
+# choices(컬렉션, 가중치, 샘플 수) : 컬렉션 값에 가중치를 지정하고 임의의 값을 추출
+d = random.choices([1, 2, 3, 4, 5], [10, 10, 20, 20, 15])
+print(f"{d}")
+
+# sample(컬렉션, 샘플 수) : 컬렉션 타입의 값 중에 지정한 샘플 수 만큼 랜덤으로 추출
+e = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+f = random.sample(e, 3)
+print(f"{f}")
+
+lottoNum = random.sample(range(1, 45), 6)
+print(lottoNum)
+
+print("컴퓨터가 선택한 숫자를 맞춰보세요")
+value = int(input("1부터 10까지 숫자 중 한개를 입력해주세요\n"))
+computer = random.randint(1, 10)
+if computer == value :
+    print("맞았습니다.")
+else :
+    print(f"틀렸습니다. 컴퓨터가 선택한 숫자는 {computer} 입니다.")
